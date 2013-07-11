401adb761bc59ea8a923fd01938ed78c0b3d2b6f
sekayasin <sekayasin@gmail.com>
2013-07-11 08:16:04 +0000

    Edited web.js to output contents of index.html

--- web.js
+++ web.js
@@ -7,7 +7,7 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send(buffer.toString);
+  response.send(buffer.toString());
 });
 
 var port = process.env.PORT || 5000;
